5955813ee9aa0728e68cd2e90b0a5af3e5cc42dc
Krasimir Nikolov <wirert@gmail.com>
2023-07-17 12:09:51 +0300

    small corrections to add view

--- PrintingHouse.Core/Models/Article/AllArticleViewModel.cs
+++ PrintingHouse.Core/Models/Article/AllArticleViewModel.cs
@@ -1,19 +1,24 @@
 ﻿namespace PrintingHouse.Core.Models.Article
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class AllArticleViewModel
     {
         public Guid Id { get; set; }
        
         public string Name { get; set; } = null!;
-       
+
+        [Display(Name = "Design file name")]
         public string ImageName { get; set; } = null!;
 
         public string Material { get; set; } = null!;
 
+        [Display(Name ="Color model")]
         public string? ColorModel { get; set; }
 
         public int ClientId { get; set; }
-       
+
+        [Display(Name = "Client name")]
         public string ClientName { get; set; } = null!;
     }
 }
